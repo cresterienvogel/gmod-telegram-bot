21e7b6da689dd97141f3c17e39dc64b1c9cc1667
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-08-24 15:26:35 +0700

    Update logs.lua

--- lua/telegram/lib/logs.lua
+++ lua/telegram/lib/logs.lua
@@ -1,27 +1,3 @@
-function Telegram.CreateDayLog(date, id)
-    if not file.Exists("telegram", "DATA") then
-        file.CreateDir("telegram")
-    end
-
-    if not file.Exists("telegram/logs", "DATA") then
-        file.CreateDir("telegram/logs")
-    end
-
-    file.Append("telegram/logs/" .. date .. ".txt", id .. " ")
-end
-
 function Telegram.IsMessageSent(date, id)
-    if not file.Exists("telegram", "DATA") then
-        return false
-    end
-
-    if not file.Exists("telegram/logs", "DATA") then
-        return false
-    end
-
-    if not file.Exists("telegram/logs/" .. date .. ".txt", "DATA") then
-        return false
-    end
-
-    return tobool(string.find(file.Read("telegram/logs/" .. date .. ".txt"), id))
+    return os.time() - date > 30 and true or false
 end
